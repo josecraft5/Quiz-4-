43a81adee5ac8b8bd89ab7dbf1823aa7feadf2b2
Killua_420 <74071608+josecraft5@users.noreply.github.com>
2026-04-28 20:44:47 -0500

    Cambios ya con dealer al main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-class jugador:
+class Jugador:
     def __init__(self,nombre):
         self.nombre = nombre
         self.cartas = [11,2,3,4,5,6,7,8,9,10,10,10]
@@ -12,29 +12,69 @@ class jugador:
         self.set_cartas.append(carta_1)
         self.set_cartas.append(carta_2)
         print("El Dealer esta barajando...")
-        print(f"El Dealer a sacado {self.set_cartas}, Suma -> {sum(self.set_cartas)}")       
+        print(f"Tu sacas {self.set_cartas}, Suma -> {sum(self.set_cartas)}")      
+        return {sum(self.set_cartas)} 
     def suma(self):
         return sum(self.set_cartas)
-    def mostrar_mano_suma(self):
-        print(f"Suma de tus cartas = {sum(self.set_cartas)}")
+    
     def pedir_carta_adicional(self):
-        carta_add = input("Deseas pedir otra carta? (Yes/No): ").lower()
-        while carta_add != "yes" and carta_add != "no":
-            carta_add = input("Error elije (Yes/No): ").lower()
-        if carta_add == "yes":
-            carta_n = random.choice(self.cartas)
-            self.set_cartas.append(carta_n)
-            print(f"El Dealer saca {self.set_cartas}, Suma -> {sum(self.set_cartas)}")
+        carta_add = input("¿Deseas pedir otra carta? (Yes/No): ").lower()
+
+        while carta_add != "no":
+
+            while carta_add != "yes" and carta_add != "no":
+                carta_add = input("Error, elige (Yes/No): ").lower()
+
+            if carta_add == "yes":
+                carta_n = random.choice(self.cartas)
+                self.set_cartas.append(carta_n)
+
+                print(f"Tu sacas {self.set_cartas}, Suma -> {sum(self.set_cartas)}")
+
+                if sum(self.set_cartas) > 21:
+                    print(f"Te pasaste del 21: {self.set_cartas}, Suma -> {sum(self.set_cartas)}")
+                    return {sum(self.set_cartas)}
+                    break
+
+            carta_add = input("¿Deseas pedir otra carta? (Yes/No): ").lower()
+
+        if carta_add == "no":
+            print(f"Te plantaste con {self.set_cartas}, Suma -> {sum(self.set_cartas)}")
+            return {sum(self.set_cartas)}
+                
+
     def calcular_puntos(self):
         if sum(self.set_cartas) == 21:
             print(f"Congratulaciones, {self.nombre} hiciste un BLACKJACK!!!") 
         elif sum(self.set_cartas) < 21:
             print(f"Congratulaciones, {self.nombre} Ganaste con {self.set_cartas} ") 
         elif sum(self.set_cartas) > 21:
-            print(f"Perdista, {self.nombre} te quedaste con {self.set_cartas}: ") 
+            print(f"Perdiste, {self.nombre} te quedaste con {self.set_cartas}: ") 
     
 def separador(caracter="=",longuitud = 60):
     return print(caracter*longuitud)    
+
+class Dealer(Jugador):
+    def __init__(self, nombre):
+        super().__init__(nombre)
+        self.puntos = 99999999
+    def pedir_carta_inicial(self):
+        carta_1 = random.choice(self.cartas)
+        carta_2 = random.choice(self.cartas)
+        self.set_cartas.append(carta_1)
+        self.set_cartas.append(carta_2)
+        print(f"El Dealer saca {self.set_cartas}, Suma -> {sum(self.set_cartas)}")       
+    def pedir_carta(self):
+        carta_9 = random.choice(self.cartas)
+        self.set_cartas.append(carta_9)
+    def game_dealer(self):
+        while sum(self.set_cartas) < 17:
+            self.pedir_carta()
+            print(f"El Dealer pide y saca {self.set_cartas}, Suma -> {sum(self.set_cartas)}")   
+        if sum(self.set_cartas) == 17:
+            print(f"El Dealer se planta con {self.set_cartas}, Suma -> {sum(self.set_cartas)}")   
+        else:
+            print(f"Dealer se pasó de 21 con {self.set_cartas}, Suma -> {sum(self.set_cartas)}")
 
 def game():    
     separador()
@@ -42,19 +82,22 @@ def game():
     separador()
     # nombre = input("Cual es tu nombre?: ")
     nombre = "jose"
-    p1 = jugador(nombre)
+    nombre_dealer = "DEALER"
+    p1 = Jugador(nombre)
+    d1 = Dealer(nombre_dealer)
     print(f"Tienes {p1.puntos} creditos a tu favor")
+
     start = input("Comenzar a jugar (Yes/No): ").lower()
     while start != "yes" and start != "no":
         start = input("Error elije (Yes/No): ").lower()
+
     if start == "yes":
         p1.pedir_carta_inicial()
         p1.pedir_carta_adicional()
-        p1.pedir_carta_adicional()
-        p1.pedir_carta_adicional()
-        p1.calcular_puntos()
+        d1.pedir_carta_inicial()
+        d1.game_dealer()
+
     elif start == "no":
         print("El juego a Finalizado")
-    
     
 game()
